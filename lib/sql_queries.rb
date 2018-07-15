5202b228d955a07cdd3b69c52884f7aa8048800a
Kevin Chi <kevinchi118@gmail.com>
2018-07-15 20:34:46 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,10 @@ ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT "
+"SELECT projects.category, SUM(pledges.amount)
+FROM pledges INNER JOIN projects
+ON pledges.project_id = projects.id
+GROUP BY projects.category HAVING projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
